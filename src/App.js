8f75f4b9bffbf6b1787b424fa52c42c2353478eb
Italo1994 <italohortiz2015@gmail.com>
2022-07-04 03:33:13 -0300

    Consertando o bug da função assíncrona dentro do useEffect e criando a função favoritar que seleciona um repositório como favorito

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch('https://api.github.com/users/Italo1994/repos');
-    const data = await response.json();
+  useEffect( () => {
 
-    setRepositories(data);
+    (async () => {
+      const response = await fetch('https://api.github.com/users/Italo1994/repos');
+      const data = await response.json();
+
+      setRepositories(data);
+    })()
+
   }, []);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map( repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    } );
+
+    setRepositories(newRepositories);
+  }
 
   return ( 
       <ul>
         {repositories.map(repo => ( 
-          <li key={repo.id}> {repo.name} </li> 
+          <>
+            <li key={repo.id}> 
+              { repo.name }
+              { repo.favorite && <span>(Favorite)</span> }
+            </li>
+            <button onClick={ () => handleFavorite(repo.id) }>Favoritar</button>
+          </>
         ))}
       </ul>  
   );
