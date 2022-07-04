4b2605bc57a924c19375bc162ec4b138ede0cabc
Italo1994 <italohortiz2015@gmail.com>
2022-07-03 23:48:58 -0300

    criando o projeto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/Italo1994/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  return ( 
+      <ul>
+        {repositories.map(repo => ( 
+          <li key={repo.id}> {repo.name} </li> 
+        ))}
+      </ul>  
   );
 }
-
-export default App;
